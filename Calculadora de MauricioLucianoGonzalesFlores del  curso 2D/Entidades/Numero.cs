00b10f278a0da7e67484646922a8c5dea3fdad82
MauriciofloresGonales <mlgf2000@hotmail.com>
2020-09-13 23:22:05 -0300

    fix(miCalculadora): tp not finished

---
+++ Calculadora de MauricioLucianoGonzalesFlores del  curso 2D/Entidades/Numero.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        double numeroIngresado;
+
+        public Numero()
+        {
+            this.numeroIngresado = 0;
+        }
+        public double NumeroIngresado
+        {
+            get { return this.numeroIngresado; }
+            set { this.numeroIngresado = value; }
+        }
+
+        private double ValidarNumero(string numero)
+        {
+            double retorno = 0;
+            if(double.TryParse(numero, out retorno))
+            {
+                retorno = Convert.ToDouble(retorno);
+            }
+            return retorno;
+        }
+
+        public void SetNumero(string numero)
+        {
+            NumeroIngresado = ValidarNumero(numero);
+        }
+
+        private bool EsBinario(string numeroBinario)
+        {
+            bool retorno = true;
+            char caracterCero = '0';
+            char caracterUno = '1';
+            char caracterComa = ',';
+            char caracterPunto = '.';
+
+            for (int i = 0; i < numeroBinario.Length; i++)
+            {
+                Console.WriteLine($"La letra es: {numeroBinario[i]}");
+                if (numeroBinario[i] != caracterCero && numeroBinario[i] != caracterUno)
+                {
+                    if (numeroBinario[i] != caracterComa && numeroBinario[i] != caracterPunto)
+                    {
+                        retorno = false;
+                        break;
+                    }
+                }
+            }
+            return retorno;
+        }
+
+        public string BinarioDecimal(double numero)
+        {
+            decimal numeroFinal;
+            string numeroEnString = numero.ToString();
+
+            if (EsBinario(numeroEnString))
+            {
+                numeroFinal = (decimal)numero;
+                return numeroEnString = numeroFinal.ToString();
+            }
+            else
+            {
+                return "Valor invalido";
+            }
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            int auxNumero = (int)numero;
+            string numeroBinario = "";
+            if (auxNumero > 0)
+            {
+                while (auxNumero > 0)
+                {
+                    if (auxNumero % 2 == 0)
+                    {
+                        numeroBinario = "0" + numeroBinario;
+                    }
+                    else
+                    {
+                        numeroBinario = "1" + numeroBinario;
+                    }
+                    auxNumero = (int)(auxNumero / 2);
+                }
+                return numeroBinario;
+            }
+            else
+            {
+                if (auxNumero == 0)
+                {
+                    numeroBinario = "0";
+                }
+                else
+                {
+                    numeroBinario = "Valor invalido";
+                }
+            }
+            return numeroBinario;
+        }
+    }
+}
---
+++ Calculadora de MauricioLucianoGonzalesFlores del  curso 2D/MiCaluladora/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCaluladora
+{
+    public partial class MiCalculadora : Form
+    {
+        Numero operadorUno = new Numero();
+        Numero operadorDos = new Numero();
+        char operador;
+        string resultdado;
+        public MiCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNumero1_TextChanged(object sender, EventArgs e)
+        {
+            string numero = this.txtNumero1.Text;
+            operadorUno.SetNumero(numero);
+        }
+
+        private void txtNumero2_TextChanged(object sender, EventArgs e)
+        {
+            string numero = this.txtNumero2.Text;
+            operadorUno.SetNumero(numero);
+        }
+
+        private void cmbOperador_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string resultado = this.cmbOperador.Text;
+            operador = resultado[0];
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            resultdado = Calculadora.operar(operador, operadorUno, operadorDos);
+            lblResultado_TextChanged(sender, e);
+        }
+
+        private void lblResultado_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = resultdado;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            string numeroUno = "0";
+            string numeroDos = "0";
+            operadorUno.SetNumero(numeroUno);
+            operadorDos.SetNumero(numeroDos);
+        }
+    }
+}
