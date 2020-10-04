99b2bb910ff07cfb17a9bfb7b5b3c59c2dadc763
MauriciofloresGonales <mlgf2000@hotmail.com>
2020-10-04 20:54:04 -0300

    feat(TP-02): tp 2 terminado

---
+++ TP-02/Entidades/Sedan.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo 
+        { 
+            CuatroPuertas,
+            CincoPuertas 
+        }
+
+        ETipo tipo;
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        public sealed override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color) :base(chasis, marca, color)
+        {
+            this.tipo = ETipo.CuatroPuertas;
+        }
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo) : base(chasis, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.Append($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine($"TIPO : {this.tipo}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
